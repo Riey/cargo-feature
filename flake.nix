337a81052efd556d250b22a401e47bc90ee798fd
Riey <creeper844@gmail.com>
2022-04-30 16:01:19 +0900

    Use nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+    description = "erars";
+
+    inputs = {
+        nixpkgs.url = github:NixOS/nixpkgs;
+        flake-utils.url = github:numtide/flake-utils;
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem
+            (system:
+                let
+                    pkgs = nixpkgs.legacyPackages.${system};
+                in
+                {
+                    devShell = pkgs.mkShell {
+                        name = "cargo-feature-shell";
+                        nativeBuildInputs = with pkgs; [
+                            pkg-config
+                            rustfmt
+                            rustc
+                            cargo
+                        ];
+                        RUST_BACKTRACE=1;
+                    };
+                    defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+                        pname = "cargo-feature";
+                        version = "0.6.0";
+
+                        src = ./.;
+
+                        cargoLock = {
+                            lockFile = ./Cargo.lock;
+                        };
+
+                        meta = with pkgs.lib; {
+                            description = "Cargo plugin to manage dependency features";
+                            homepage = "https://github.com/Riey/cargo-feature";
+                            license = licenses.mit;
+                            platforms = platforms.unix;
+                            maintainers = with maintainers; [ riey ];
+                        };
+                    };
+                }
+            );
+}
